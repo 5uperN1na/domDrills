3cd014c716be7a227b9990a63645e525dcee3564
Paula Suarez <pauladzuck@hotmail.com>
2018-08-01 08:15:12 -0500

    Add updates to all files in project.

---
+++ scripts.js
@@ -0,0 +1,54 @@
+
+window.addEventListener('DOMContentLoaded', function () {
+
+
+let div = document.createElement('div');
+div.className = 'header-container';
+//document.body.appendChild(div);
+
+//H1
+let h1 = document.createElement('h1');
+let h1Text = document.createTextNode('This is an h1.');
+h1.appendChild(h1Text);
+document.body.appendChild(h1);
+
+//H2
+let h2 = document.createElement('h2');
+let h2Text = document.createTextNode('This is an h2.');
+h2.appendChild(h2Text);
+document.body.appendChild(h2);
+
+//H3
+let h3 = document.createElement('h3');
+let h3Text = document.createTextNode('This is an h3.');
+h3.appendChild(h3Text);
+document.body.appendChild(h3);
+
+//H4
+let h4 = document.createElement('h4');
+let h4Text = document.createTextNode('This is an h4.');
+h4.appendChild(h4Text);
+document.body.appendChild(h4);
+
+//H5
+let h5 = document.createElement('h5');
+let h5Text = document.createTextNode('This is an h5.');
+h5.appendChild(h5Text);
+document.body.appendChild(h5);
+
+//H6
+let h6 = document.createElement('h6');
+let h6Text = document.createTextNode('This is an h6.');
+h6.appendChild(h6Text);
+document.body.appendChild(h6);
+
+
+
+
+
+
+
+
+
+
+});
